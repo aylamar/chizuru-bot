ff67b07456ff048e47ae9f19ed0057eaf1a9b345
aylamar <aylamar@users.noreply.github.com>
2021-08-01 19:15:32 -0500

    GetTwitchToken now creates token.json file if needed

--- src/util/GetTwitchToken.ts
+++ src/util/GetTwitchToken.ts
@@ -11,22 +11,29 @@ async function getTwitchToken() {
         token_type: string
     }
 
-    let rawdata: any = fs.readFileSync('./tokens.json')
-    let data: IsToken = await JSON.parse(rawdata)
-    
-    if (data.expire_time <= Date.now()) {
-        console.log('Fetching new token...')
-        var res: any = await fetch(`https://id.twitch.tv/oauth2/token?&client_id=${twitchClientID}&client_secret=${twitchClientSecret}&grant_type=client_credentials`, { method: 'POST' })
-        res = await res.json()
-        data["access_token"] = res.access_token
+    try {
+        let rawdata: any = fs.readFileSync('./token.json')
+        let data: IsToken = await JSON.parse(rawdata)
+        
+        if (data.expire_time <= Date.now()) {
+            console.log('Fetching new token...')
+            var res: any = await fetch(`https://id.twitch.tv/oauth2/token?&client_id=${twitchClientID}&client_secret=${twitchClientSecret}&grant_type=client_credentials`, { method: 'POST' })
+            res = await res.json()
+            data["access_token"] = res.access_token
 
-        // Read data from "tokens.json", parse, then save
+            // Read data from "tokens.json", parse, then save
+            data.expire_time = data.expires_in + Date.now()
+            fs.writeFileSync('./token.json', JSON.stringify(data))
+            return data.access_token
+        } else {
+            //console.log('Token still valid, not fetching token.')
+            return data.access_token
+        }
+    } catch {
+        var res: any = await fetch(`https://id.twitch.tv/oauth2/token?&client_id=${twitchClientID}&client_secret=${twitchClientSecret}&grant_type=client_credentials`, { method: 'POST' })
+        let data = await res.json()
         data.expire_time = data.expires_in + Date.now()
-        fs.writeFileSync('./tokens.json', JSON.stringify(data))
-        return data.access_token
-    } else {
-        //console.log('Token still valid, not fetching token.')
-        return data.access_token
+        fs.writeFileSync('./token.json', JSON.stringify(data))
     }
 }
 

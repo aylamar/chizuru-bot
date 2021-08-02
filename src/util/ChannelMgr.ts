8f09fa77e18031eb54f384c9030864471d3f6b1a
aylamar <aylamar@users.noreply.github.com>
2021-08-02 18:20:37 -0500

    Added delete stream functionality

--- src/util/ChannelMgr.ts
+++ src/util/ChannelMgr.ts
@@ -39,4 +39,32 @@ ChannelMgr.addStream = function(id: string, stream: string) {
     // TODO: Check if streamer exists in streamer db, if not, add to db
 }
 
+ChannelMgr.delStream = function(id: string, stream: string) {
+    Channel.find({_id: id, followed_channels: {$in: stream}}).then((res: any) => {
+        // If no result found
+        if (res.length === 0) {
+            console.log(`${stream} does not exist in this channel`)
+        } else if (res[0].followed_channels.length === 1 && res[0].followed_channels[0] == stream) {
+            Channel.findOneAndDelete({_id: id}, function(err: any, result: any) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(`Successfully removed ${stream} from #${id} & cleared from db`)
+                }
+            })
+        } else {
+            res[0].followed_channels = res[0].followed_channels.filter((strm: string) => strm !== stream)
+            res[0].save(function(err: any, result: any) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(`Successfully removed ${stream} from #${id}`)
+                }
+            })
+            console.log(`Removed ${stream} from ${id}`)
+        }
+    })
+    // TODO: Check if streamer exists in any other channels, if not, delete streamer 
+}
+
 export default ChannelMgr

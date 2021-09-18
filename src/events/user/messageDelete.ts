848bb0175198b746c0e3cbe93c2de2e1029bdcc2
aylamar <aylamar@users.noreply.github.com>
2021-09-18 00:12:47 -0500

    feat: deleted message logging

---
+++ src/events/user/messageDelete.ts
@@ -0,0 +1,39 @@
+import { Message, MessageEmbed } from 'discord.js'
+import { Bot } from '../../client/client'
+import { RunFunction } from '../../interfaces/Event'
+import { getGuild } from '../../util/Guild'
+
+export const run: RunFunction = async (client: Bot, message: Message) => {
+    let guildID: string = message.guildId
+    let logChannel: string = null
+
+    if (!client.cache[guildID]) {
+        let data = await getGuild(guildID)
+        client.cache[guildID] = data
+        logChannel = data.logChannel
+    } else {
+        logChannel = client.cache[guildID].logChannel
+    }
+    if (!logChannel) return
+
+    let channel = client.channels.resolve(logChannel)
+    if (channel.isText()) {
+        let embed = new MessageEmbed()
+            .setAuthor(message.author.tag, message.author.avatarURL())
+            .setDescription(`Message from <@${message.author.id}> deleted in <#${message.channelId}>\n\n${message.content}`)
+            .setColor(client.colors.warn)
+            .setFooter(`User ID: ${message.author.id}`)
+            .setTimestamp()
+
+        try {
+            channel.send({ embeds: [embed] })
+        } catch (err) {
+            client.logger.error(err)
+        }
+        return
+    } else {
+        return
+    }
+}
+
+export const name: string = 'messageDelete'

fc90cb415282346aa5e675cb446a73f5dbcf0e5e
aylamar <aylamar@users.noreply.github.com>
2022-09-16 18:59:01 -0500

    feat: add guild stats

--- src/commands/util/stats.ts
+++ src/commands/util/stats.ts
@@ -1,7 +1,9 @@
+import { ApplicationCommandOptionType, ChatInputCommandInteraction, EmbedBuilder, Guild } from 'discord.js';
 import { Chizuru } from '../../interfaces';
 import { prisma } from '../../services';
+import { Bot } from '../../structures/bot';
 import { Command } from '../../structures/command';
-import { generateEmbed, replyEmbed, replyMessage } from '../../utils';
+import { deferReply, generateEmbed, replyEmbed } from '../../utils';
 
 export default new Command({
     name: 'stats',
@@ -10,52 +12,93 @@ export default new Command({
     dmPermission: false,
     defaultMemberPermissions: ['SendMessages'],
     module: Chizuru.CommandModule.Global,
-    options: [],
+    options: [
+        {
+            name: 'user',
+            description: 'Shows stats about yourself',
+            type: ApplicationCommandOptionType.Subcommand,
+        },
+        {
+            name: 'guild',
+            description: 'Shows stats about the guild',
+            type: ApplicationCommandOptionType.Subcommand,
+        }
+    ],
 
     execute: async (client, interaction) => {
-        if (!interaction.inGuild()) return;
-        let user = await getUser(interaction.user.id);
-        if (!user) return await replyMessage(interaction, 'You are not registered in the database, have you sent any messages before?');
+        if (!interaction.inCachedGuild()) return;
+        let embed: Promise<EmbedBuilder>;
+        const subcommand = interaction.options.getSubcommand();
+        const defer = deferReply(interaction);
 
-        let rawChannelStats = getRawChannelStats(interaction.user.id, interaction.guildId);
-        let totalMessages = getTotalMessages(interaction.user.id);
+        switch (subcommand) {
+            case 'guild':
+                embed = handleGuildStats(client, interaction);
+                break;
+            case 'user':
+                embed = handleUserStats(client, interaction);
+                break;
+            default:
+                return;
+        }
 
-        let channelStats = await generateChannelStats(await rawChannelStats);
-
-        let serverStats: Chizuru.Field = {
-            name: `${ interaction.guild?.name } Message Stats`,
-            value: channelStats.message + (channelStats.otherMsgCount > 0 ? `\n\n And ${ channelStats.otherMsgCount } `
-                + `${ channelStats.otherMsgCount == 1 ? 'message' : 'messages' } across ${ channelStats.channelCount - 5 } `
-                + ` other ${ channelStats.channelCount == 6 ? 'channel' : 'channels' }` : ''),
-            inline: false,
-        };
-
-        let userStats: Chizuru.Field = {
-            name: 'User Stats',
-            value: `Tracking since ${ await convertDate(user.created) }\n` + `Total Messages Sent: ${ (await totalMessages) ? await totalMessages : 0 }`,
-            inline: true,
-        };
-
-        let userInfo: Chizuru.Field = {
-            name: 'User Info',
-            value: `Username: ${ interaction.user.username }\n` + `Discriminator: ${ interaction.user.discriminator }\n`
-                + `Avatar: [Click Here](${ interaction.user.displayAvatarURL() })\n` + `User ID: ${ interaction.user.id }\n`
-                + `Create Date: ${ await convertDate(interaction.user.createdAt) }\n`,
-            inline: true,
-        };
-
-        let embed = generateEmbed({
-            title: 'Stats',
-            fields: [userInfo, userStats, serverStats],
-            color: client.colors.purple,
-            author: interaction.user.tag,
-            authorIcon: interaction.user.displayAvatarURL(),
-        });
-
-
+        await defer;
         return await replyEmbed(interaction, await embed);
     },
 });
+
+async function handleGuildStats(client: Bot, interaction: ChatInputCommandInteraction<'cached'>): Promise<EmbedBuilder> {
+    const guild = interaction.guild;
+    const channelField = generateChannelStats(guild.id);
+    const userField = generateUserStats(guild.id);
+    const guildStats = generateGuildStats(guild);
+
+    return generateEmbed({
+        title: `Stats for ${ guild.name }`,
+        color: client.colors.blurple,
+        authorIcon: interaction.guild.iconURL() || undefined,
+        fields: [await guildStats, await channelField, await userField],
+    });
+}
+
+async function handleUserStats(client: Bot, interaction: ChatInputCommandInteraction<'cached'>): Promise<EmbedBuilder> {
+    const user = await prisma.user.findUnique({
+        where: { userId: interaction.user.id }, include: { guilds: true },
+    });
+    if (!user) return generateEmbed({
+        title: 'Error', msg: 'You are not registered in the database', color: client.colors.error,
+    });
+    let rawChannelStats = getRawChannelStats(interaction.user.id, interaction.guildId);
+    let totalMessages = getUserTotalMessages(interaction.user.id);
+
+    let channelStats = await generateUserChannelStats(await rawChannelStats);
+
+    let serverStats: Chizuru.Field = {
+        name: `${ interaction.guild?.name } Message Stats`,
+        value: channelStats.message + (channelStats.otherMsgCount > 0 ? `\n\n And ${ channelStats.otherMsgCount } ` + `${ channelStats.otherMsgCount == 1 ? 'message' : 'messages' } across ${ channelStats.channelCount - 5 } ` + ` other ${ channelStats.channelCount == 6 ? 'channel' : 'channels' }` : ''),
+        inline: false,
+    };
+
+    let userStats: Chizuru.Field = {
+        name: 'User Stats',
+        value: `Tracking since ${ await convertDate(user.created) }\n` + `Total Messages Sent: ${ (await totalMessages) ? await totalMessages : 0 }`,
+        inline: true,
+    };
+
+    let userInfo: Chizuru.Field = {
+        name: 'User Info',
+        value: `Username: ${ interaction.user.username }\n` + `Discriminator: ${ interaction.user.discriminator }\n` + `Avatar: [Click Here](${ interaction.user.displayAvatarURL() })\n` + `User ID: ${ interaction.user.id }\n` + `Create Date: ${ await convertDate(interaction.user.createdAt) }\n`,
+        inline: true,
+    };
+
+    return generateEmbed({
+        title: 'Stats',
+        fields: [userInfo, userStats, serverStats],
+        color: client.colors.purple,
+        author: interaction.user.tag,
+        authorIcon: interaction.user.displayAvatarURL(),
+    });
+}
 
 async function convertDate(input: Date) {
     let date = new Date(input);
@@ -65,60 +108,113 @@ async function convertDate(input: Date) {
     return `${ year }-${ month }-${ day }`;
 }
 
-async function getRawChannelStats(userId: string, guildId: string): Promise<ChannelStats[]> {
+async function getRawChannelStats(userId: string, guildId: string): Promise<flatChannel[]> {
     let stats = await prisma.messageStats.groupBy({
-        by: ['channelId'],
-        _sum: { messageCount: true },
-        orderBy: { _sum: { messageCount: 'desc' } },
-        where: {
-            userId: userId,
-            channel: { guildId: guildId },
+        by: ['channelId'], _sum: { messageCount: true }, orderBy: { _sum: { messageCount: 'desc' } }, where: {
+            userId: userId, channel: { guildId: guildId },
         },
     });
 
     // convert to stats to array of { channelId: channelId, messageCount: messageCount }
     return stats.map((item: any) => {
-        return { channelId: item.channelId, messageCount: item._sum.messageCount };
+        return { channelId: item.channelId, count: item._sum.messageCount };
     });
 }
 
-async function getTotalMessages(userId: string): Promise<number | null> {
+async function getUserTotalMessages(userId: string): Promise<number | null> {
     let totalMessages = await prisma.messageStats.aggregate({
-        _sum: { messageCount: true },
-        where: {
+        _sum: { messageCount: true }, where: {
             userId: userId,
         },
     });
     return totalMessages._sum.messageCount;
 }
 
-async function getUser(userId: string) {
-    return prisma.user.findUnique({
-        where: {
-            userId: userId,
-        }, include: {
-            guilds: true,
-        },
+async function generateGuildStats(guild: Guild): Promise<Chizuru.Field> {
+    const messageStats = prisma.messageStats.aggregate({
+        where: { channel: { guildId: guild.id } }, _sum: { messageCount: true },
     });
+
+    const totalUsers = prisma.user.aggregate({
+        where: { guilds: { some: { guildId: guild.id } } }, _count: { _all: true },
+    });
+
+    // get guild creation date
+    const creationDate = new Date(guild.createdAt);
+    const creationDateStr = `${ creationDate.getFullYear() }-${ pad(creationDate.getMonth() + 1) }-${ pad(creationDate.getDate()) }`;
+
+    return {
+        name: 'Guild Stat',
+        value: `Guild: ${ creationDateStr }\n` + `Total Messages: ${ (await messageStats)._sum.messageCount }\n` + `Total Users: ${ (await totalUsers)._count._all }\n`,
+        inline: true,
+    };
 }
 
+async function generateChannelStats(guildId: string): Promise<Chizuru.Field> {
+    const channels = await prisma.messageStats.groupBy({
+        by: ['channelId'], where: { channel: { guildId: guildId } }, _sum: { messageCount: true },
+    });
 
-async function generateChannelStats(channelStats: ChannelStats[]): Promise<parsedChannelStats> {
+    let flatChannels: flatChannel[];
+    flatChannels = channels.map((channel) => {
+        return {
+            channelId: channel.channelId, count: channel._sum.messageCount ? channel._sum.messageCount : 0,
+        };
+    });
+
+    // get top 5 channels
+    flatChannels.sort((a, b) => b.count - a.count);
+    flatChannels = flatChannels.slice(0, 5);
+
+    const channelArr = flatChannels.map((channel) => {
+        return `<#${ channel.channelId }>: ${ channel.count } messages`;
+    });
+
+    const channelStr = channelArr.join('\n');
+    return {
+        name: 'Top Channels', value: channelStr.length > 0 ? channelStr : 'No channels found..', inline: true,
+    };
+}
+
+async function generateUserStats(guildId: string): Promise<Chizuru.Field> {
+    const users = await prisma.messageStats.groupBy({
+        by: ['userId'], where: { channel: { guildId: guildId } }, _sum: { messageCount: true },
+    });
+
+    let flatUsers: flatUser[];
+    flatUsers = users.map((user) => {
+        return {
+            userId: user.userId, count: user._sum.messageCount ? user._sum.messageCount : 0,
+        };
+    });
+
+    // get top 5 users
+    flatUsers.sort((a, b) => b.count - a.count);
+    flatUsers = flatUsers.slice(0, 5);
+
+    const userArr = flatUsers.map((user) => {
+        return `<@${ user.userId }>: ${ user.count } messages`;
+    });
+
+    const userStr = userArr.join('\n');
+    return {
+        name: 'Top Users', value: userStr.length > 0 ? userStr : 'No users found..', inline: true,
+    };
+}
+
+async function generateUserChannelStats(channelStats: flatChannel[]): Promise<parsedChannelStats> {
     let data: parsedChannelStats = {
-        topMsgCount: 0,
-        otherMsgCount: 0,
-        channelCount: 0,
-        message: '',
+        topMsgCount: 0, otherMsgCount: 0, channelCount: 0, message: '',
     };
 
     for (let channel of channelStats) {
-        if (channel.messageCount === null) continue;
+        if (channel.count === null) continue;
         if (data.channelCount < 5) {
-            data.message += `<#${ channel.channelId }> ${ channel.messageCount } messages\n`;
+            data.message += `<#${ channel.channelId }> ${ channel.count } messages\n`;
         } else {
-            data.otherMsgCount += channel.messageCount;
+            data.otherMsgCount += channel.count;
         }
-        data.topMsgCount += channel.messageCount;
+        data.topMsgCount += channel.count;
         data.channelCount++;
     }
     // remove last \n from data.message
@@ -126,9 +222,18 @@ async function generateChannelStats(channelStats: ChannelStats[]): Promise<parse
     return data;
 }
 
-interface ChannelStats {
+function pad(num: number): string {
+    return num < 10 ? `0${ num }` : `${ num }`;
+}
+
+interface flatChannel {
     channelId: string,
-    messageCount: number
+    count: number
+}
+
+interface flatUser {
+    userId: string,
+    count: number
 }
 
 interface parsedChannelStats {

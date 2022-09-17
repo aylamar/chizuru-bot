63ce157b79987a00366f14dbdc1dc4f822bd0beb
aylamar <aylamar@users.noreply.github.com>
2022-09-17 12:05:12 -0500

    feat: disconnect users when leaving guild

---
+++ src/events/guild/guildMemberRemove.ts
@@ -0,0 +1,25 @@
+import { Events, GuildMember } from 'discord.js';
+import { prisma } from '../../services';
+import { Bot } from '../../structures/bot';
+import { Event } from '../../structures/event';
+
+export default new Event({
+    name: Events.GuildMemberRemove,
+    execute: async (client: Bot, member: GuildMember) => {
+        if (!member || !member.id) return;
+        try {
+            await prisma.user.update({
+                where: { userId: member.id },
+                data: { guilds: { disconnect: { guildId: member.guild.id } } },
+            });
+            client.logger.info(
+                `${ member.user.tag } (${ member.user.id }) left ${ member.guild.name }, successfully disconnected them from the guild`,
+                { label: 'event' });
+        } catch (err) {
+            client.logger.error(
+                `Left ${ member.guild.name } (${ member.guild.id }), errored while disconnected a user in the database`,
+                { label: 'event' });
+            client.logger.error(err);
+        }
+    },
+});
